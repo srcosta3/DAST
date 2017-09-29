a414c6925a6c2447cd5dd2075aee5da7cc1b5be3
ArielVe <arielvelascoalves@gmail.com>
2017-09-29 15:51:28 -0300

    Primeira parte
    login com css

---
+++ foxtrot/home.php
@@ -0,0 +1,17 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="estiloPI.css">
+<title>Home</title>
+</head>
+<body>
+	<div id="cor1"></div>
+	<div id="cor2"></div>
+	<div id="inicio">
+		<img id="logo2" src="LogoFoxtrot.jpg">
+		<h1>Sobre</h1>
+	</div>
+	
+</body>
+</html>
---
+++ foxtrot/login.php
@@ -0,0 +1,33 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="estiloPI.css">
+<title>Login</title>
+</head>
+<body>
+	<div id="cor1"></div>
+	<div id="cor2"></div>
+	<h1>Login</h1>
+	<div id="boxlogin">
+		<div id="borda1"></div>
+		<div id="borda2"></div>
+		<div id="borda3">
+			<form>
+				<label for="text" id="login">Login</label>
+				<input type="text" id="login2" placeholder="Login...">
+				<label for="text" id="senha">Senha</label>
+				<input type="password" id="senha2" placeholder="Senha...">
+				<input type="submit" value="Entrar" id="button">
+			</form>
+		</div>
+		<div id="borda4"></div>
+		<div id="borda5"></div>
+		<div id="img">
+			<img id="logo" src="LogoFoxtrot.jpg">
+		</div>
+	</div>
+	<div id="cor3"></div>
+	<div id="cor4"></div>
+</body>
+</html>
